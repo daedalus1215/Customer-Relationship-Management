f40e9a8b1dfdf8403a6e0c6591d2bc87bef42ad9
larry <ladams1776@yahoo.com>
2018-07-08 21:25:53 -0400

    CustomerDAOImpl is now autowired to the session factory

--- src/com/luv2code/springdemo/controller/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/controller/CustomerDAOImpl.java
@@ -2,11 +2,19 @@ package com.luv2code.springdemo.controller;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.entity.CustomerDAO;
 
+
+
 public class CustomerDAOImpl implements CustomerDAO{
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	@Override
 	public List<Customer> getCustomers() {
 		
