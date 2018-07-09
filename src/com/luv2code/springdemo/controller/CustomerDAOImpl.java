977d4a68f6e2c51d66a4326ab61de690c4a4fbba
larry <ladams1776@yahoo.com>
2018-07-08 21:21:00 -0400

    Create Customer Entity, CustomerDAO interface, CustomerDAOImpl

---
+++ src/com/luv2code/springdemo/controller/CustomerDAOImpl.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.entity.CustomerDAO;
+
+public class CustomerDAOImpl implements CustomerDAO{
+
+	@Override
+	public List<Customer> getCustomers() {
+		
+		return null;
+	}
+}
---
+++ src/com/luv2code/springdemo/entity/CustomerDAO.java
@@ -0,0 +1,7 @@
+package com.luv2code.springdemo.entity;
+
+import java.util.List;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}
