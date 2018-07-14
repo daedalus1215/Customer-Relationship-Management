23a773d1d0f3986b4f437dbbcb6ade4fc6034acf
larry <ladams1776@yahoo.com>
2018-07-14 11:11:14 -0400

    Create the processing method for adding a customer

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -44,5 +46,12 @@ public class CustomerController {
 		
 		theModel.addAttribute("customer", theCustomer);
 		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customerDTO) {
+		
+		
+		return "redirect:/customer/list";
 	}
 }
