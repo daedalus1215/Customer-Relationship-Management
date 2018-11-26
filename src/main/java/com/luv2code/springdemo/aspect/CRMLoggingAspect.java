92578028b0e198a39bced0c800ab26fccede6e57
larry <ladams1776@yahoo.com>
2018-11-25 19:14:51 -0500

    Setup our Logging Aspect.

---
+++ src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+    //@todo: setup logger
+    //@todo: setup pointcut declarations
+    //@todo: add @Before advice
+    //@todo: add @AfterReturning advice
+}
