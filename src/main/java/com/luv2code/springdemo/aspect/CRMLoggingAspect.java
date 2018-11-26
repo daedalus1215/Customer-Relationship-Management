394d495996a5997b393e12d29110f6d66f04a4dc
larry <ladams1776@yahoo.com>
2018-11-26 06:36:08 -0500

    finished with the Logging AOP

--- src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -1,14 +1,56 @@
 package com.luv2code.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 
 @Aspect
 @Component
 public class CRMLoggingAspect {
-    //@todo: setup logger
-    //@todo: setup pointcut declarations
-    //@todo: add @Before advice
-    //@todo: add @AfterReturning advice
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {}
+
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint) {
+
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>> in @Before: calling method: " + theMethod);
+
+        Object[] args = theJoinPoint.getArgs();
+        for (Object tempArg: args) {
+            myLogger.info("=====>> argument: " + tempArg);
+        }
+    }
+
+    @AfterReturning(
+            pointcut="forAppFlow()",
+            returning="theResult"
+            )
+    public void afterReturning(JoinPoint joinPoint, Object theResult) {
+
+        String theMethod = joinPoint.getSignature().toShortString();
+        myLogger.info("=====>> in @AfterReturning: calling method: " + theMethod);
+
+        // display data returned
+        myLogger.info("======>> in @AfterReturning: result: " + theResult);
+    }
 }
