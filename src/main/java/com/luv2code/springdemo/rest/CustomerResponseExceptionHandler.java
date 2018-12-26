7793b8271a1092051bec93fbea491579eda75040
larry <ladams1776@yahoo.com>
2018-12-26 10:43:15 -0500

    filled in the handleException method for CustomerNotFoundException in the CustomerResponseExceptionHandler.

--- src/main/java/com/luv2code/springdemo/rest/CustomerResponseExceptionHandler.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerResponseExceptionHandler.java
@@ -1,8 +1,27 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomerResponseExceptionHandler {
+
+    // Add Exception Handling for CustomerNotFoundException
+    @ExceptionHandler
+    public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+
+        // Create CustomerErrorResponse
+        CustomerErrorResponse errorResponse = new CustomerErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                exc.getMessage(),
+                System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<CustomerErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
 
 }
